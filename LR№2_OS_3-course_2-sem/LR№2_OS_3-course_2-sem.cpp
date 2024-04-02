6f7e4b8b59a36f16a5f70f506168118077c8e167
admin <admin@DESKTOP-8AJLDHS>
2024-04-02 19:39:52 +0300

    Add project files.

---
+++ LR№2_OS_3-course_2-sem/LR№2_OS_3-course_2-sem.cpp
@@ -0,0 +1,30 @@
+﻿#include <Windows.h>
+
+int main() {
+    HKEY hKey;
+    DWORD dwValue = 1;
+
+    // Вимкнення контекстного меню Панелі завдань
+    RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+        TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer"),
+        0,
+        KEY_SET_VALUE,
+        &hKey);
+
+    RegSetValueEx(hKey, TEXT("NoTrayContextMenu"), 0, REG_DWORD, (BYTE*)&dwValue, sizeof(DWORD));
+
+    RegCloseKey(hKey);
+
+    // Вимкнення контекстного меню Провідника файлів
+    RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+        TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer"),
+        0,
+        KEY_SET_VALUE,
+        &hKey);
+
+    RegSetValueEx(hKey, TEXT("NoViewContextMenu"), 0, REG_DWORD, (BYTE*)&dwValue, sizeof(DWORD));
+
+    RegCloseKey(hKey);
+
+    return 0;
+}
